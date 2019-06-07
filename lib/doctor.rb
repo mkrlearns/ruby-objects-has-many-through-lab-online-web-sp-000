d3f8bc0680a4154257b51398b6139df21248ab95
ryan m <contact@ryanmeek.com>
2019-06-07 17:53:33 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,26 +1,26 @@
-class Artist
+    
+class Doctor
   attr_accessor :name
   @@all = []
-
+  
+  def self.all
+    @@all
+  end
+  
   def initialize(name)
     @name = name
     @@all << self
   end
-
-  def self.all
-    @@all
+  
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
   end
-
-  def new_appointment(name, genre)
-    Song.new(name, self, genre)
+  
+  def appointments
+    Appointment.all.select{|appointment| appointment.doctor == self}
   end
-
-  def songs
-    Song.all.select{ |song| song.artist == self }
+  
+  def patients
+    appointments.collect{|appointment| appointment.patient}
   end
-
-  def genres
-    self.songs.collect{ |song| song.genre }
-  end
-
 end
