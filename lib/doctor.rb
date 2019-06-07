09c5c1c0e00460ad6d17a1b5aa2607d7393e553e
ryan m <contact@ryanmeek.com>
2019-06-07 17:58:16 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,25 +2,26 @@
 class Doctor
   attr_accessor :name
   @@all = []
-  
+
   def self.all
     @@all
   end
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
-  
+
   def new_appointment(patient, date)
     Appointment.new(self, date, patient)
   end
-  
+
   def appointments
     Appointment.all.select{ |appointment| appointment.doctor == self }
   end
-  
+
   def patients
     appointments.collect{ |appointment| appointment.patient }
   end
+
 end
